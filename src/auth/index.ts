14199464bb9f6a8958c696b21071767e18f85c42
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-14 08:42:56 -0300

    refactor: validateJWT - Agora retorna os dados

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,22 +1,34 @@
 import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import User from '../interfaces/user.interface';
 
 const secret = 'OvoCozido';
+
+interface Token {
+  data: {
+    id?: number,
+    username: string,
+    classe?: string,
+    level?: number,
+    password: string
+  },
+  iat: number,
+  exp: number
+}
 
 export const createJWT = (user: User) => {
   const token = jwt.sign({ data: user }, secret, { expiresIn: '1d', algorithm: 'HS256' });
   return token;
 };
 
-export const validateJWT = (req: Request, res: Response, next: NextFunction) => {
+export const validateJWT = (req: Request, res: Response) => {
   try {
     const token: string | undefined = req.headers.authorization;
     if (!token) res.status(401).json({ message: 'Token não encontrado' });
 
-    jwt.verify(token as string, secret);
+    const decoded = jwt.verify(token as string, secret) as Token;
 
-    next();
+    return decoded.data;
   } catch (err) {
     console.log(err);
     res.status(401).json({ message: 'Token inválido ou expirado' });
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import { validateJWT } from '../auth';
 
 const router = Router();
 
 const controller = new UserController();
 
 router.post('/', controller.create);
+router.get('/', validateJWT);
 
 export default router;
