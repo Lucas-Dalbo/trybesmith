95c7ad24edbbde28f88d8935cf1893eb81422fcb
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-09-14 08:46:34 -0300

    minor changes

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -4,24 +4,26 @@ import User from '../interfaces/user.interface';
 
 const secret = 'OvoCozido';
 
+interface TokenData {
+  id?: number,
+  username: string,
+  classe?: string,
+  level?: number,
+  password: string
+}
+
 interface Token {
-  data: {
-    id?: number,
-    username: string,
-    classe?: string,
-    level?: number,
-    password: string
-  },
+  data: TokenData,
   iat: number,
   exp: number
 }
 
-export const createJWT = (user: User) => {
+export const createJWT = (user: User): string => {
   const token = jwt.sign({ data: user }, secret, { expiresIn: '1d', algorithm: 'HS256' });
   return token;
 };
 
-export const validateJWT = (req: Request, res: Response) => {
+export const validateJWT = (req: Request, res: Response): TokenData | void => {
   try {
     const token: string | undefined = req.headers.authorization;
     if (!token) res.status(401).json({ message: 'Token não encontrado' });
